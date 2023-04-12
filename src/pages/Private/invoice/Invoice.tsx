5aaac8b53e80f9c8c114e81bd3335a92859eaa31
byron Rocha <by.rocha0828@gmail.com>
2023-04-11 23:08:26 -0600

    .

--- src/pages/Private/invoice/Invoice.tsx
+++ src/pages/Private/invoice/Invoice.tsx
@@ -51,13 +51,33 @@ const Invoice: FC<IProps> = (props) => {
 
   const services = [
     {
-      name: "Services Amount Educational software development in the cloud",
-      price: 600.0,
+      name: "2023-03-27",
+      hours: 1,
+      price: 15.0,
     },
+    {
+      name: "2023-03-28",
+      hours: 4,
+      price: 60.0,
+    },
+    {
+      name: "2023-03-29",
+      hours: 1,
+      price: 15.0,
+    },
+    {
+      name: "2023-03-30",
+      hours: 2,
+      price: 30.0,
+    },
+    // {
+    //   name: "Services Amount Educational software development in the cloud",
+    //   price: 600.0,
+    // },
   ];
 
   return (
-    <Container className="flex flex-row ">
+    <Container className="flex flex-row  content-center">
       <div>
         {MONTHS.map((month, index) => {
           return (
@@ -67,7 +87,7 @@ const Invoice: FC<IProps> = (props) => {
           );
         }) || <div>Months...</div>}
       </div>
-      <div className="relative  ">
+      <div className="relative m-5 ">
         <div className="static">
           <div className="absolute left-0 top-0 ">
             <ul className="flex flex-row">
@@ -104,7 +124,7 @@ const Invoice: FC<IProps> = (props) => {
           </div>
           <div
             ref={componentRef}
-            className="my-2 mx-auto h-[600px] w-[800px] grid overflow-hidden grid-lines grid-cols-2 gap-px shadow-lg"
+            className="m-5 w-[800px] grid overflow-hidden grid-lines grid-cols-2 gap-px bg-[#ffffff] text-[#000000]"
           >
             <div className="px-3 box row-start-1 col-span-2 border-b-2">
               <HeaderInvoice customer={customerDefaultState} />
@@ -114,11 +134,15 @@ const Invoice: FC<IProps> = (props) => {
                 <div className="item w-1/2 ">
                   <p className="font-semibold">Bill to</p>
                   <p className="font-bold">
-                    EduTech de Centroamérica #3101696513
+                    Compañía de Software Pura Vida. 3-101-837369
                   </p>
-                  <p>Cartago</p>
-                  <p>Costa Rica</p>
-                  <p className="mt-3">contabilidad@edutechca.com</p>
+                  {/* <p className="font-bold">
+                    EduTech de Centroamérica #3101696513
+                  </p> */}
+                  {/* <p>Cartago</p> */}
+                  <p>Cartago, La Union, Concepcion</p>
+                  <p className="mt-3">cspvfe@tlconsultores.net</p>
+                  <p className="">+506 21008013</p>
                 </div>
                 <div className="item w-1/2 ">
                   <table className="table-auto w-full shadow-none">
@@ -161,7 +185,11 @@ const Invoice: FC<IProps> = (props) => {
                 <thead>
                   <tr>
                     <th className="text-left bg-[#2560f5] text-white p-2 w-3/4">
-                      Services{" "}
+                      Date{" "}
+                    </th>
+
+                    <th className="text-right bg-[#2560f5] text-white p-2 w-1/4">
+                      Hours
                     </th>
                     <th className="text-right bg-[#2560f5] text-white p-2 w-1/4">
                       Amount
@@ -173,6 +201,7 @@ const Invoice: FC<IProps> = (props) => {
                     return (
                       <tr key={index}>
                         <td className="p-2 text-left ">{service.name}</td>
+                        <td className="p-2 text-right">{service.hours}</td>
                         <td className="p-2 text-right">
                           {UNIT + service.price + ".00"}
                         </td>
@@ -189,6 +218,9 @@ const Invoice: FC<IProps> = (props) => {
                     <tr className="">
                       <td className="py-4 text-right w-1/2">
                         <p className="font-bold">Total:</p>
+                      </td>
+                      <td className="pl-3 p-2 text-right  w-1/2">
+                        {services.reduce((a, b) => a + b.hours, 0)}
                       </td>
                       <td className="pl-3 p-2 text-right  w-1/2">
                         {UNIT +
